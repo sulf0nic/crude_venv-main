acac8a4b7a19916e836e7f117714f57b940f52c2
sulf0nic <rd.arenas@ebc.edu.mx>
2022-07-21 19:58:04 -0500

    Cambios en carpetas imagenes

--- crude_venv/settings.py
+++ crude_venv/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-5&q*=&g0zi$kjny7i--ome3gsi5sjyvpng!++a9je19_stt0mx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','127.0.0.1','rdarenas.pythonanywhere.com']
 
 
 # Application definition
